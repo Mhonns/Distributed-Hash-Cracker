e08d64c111cea8d58cf6ff5d740facee381a2d03
Mhonns <nsamaira@cmkl.ac.th>
2024-11-05 14:38:15 +0700

    Add the sha512 template

---
+++ src/sha512.py
@@ -0,0 +1,8 @@
+def padding(message : str, length : int):
+    return result
+
+
+
+if __name__ == "__main__":
+    message = input("Enter the message here: ")
+    pad_result = padding(message, len(message))
