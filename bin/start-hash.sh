4a06a87d4933162efd6e84e921b602fbd66b9e37
root <root@dev.lab.cmkl.ai>
2024-11-10 18:12:34 +0700

    Upload stable start-hash

--- bin/start-hash.sh
+++ bin/start-hash.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+HASHED='$6$1UNfGmmL8v6itaPG$j54ho2EN0nnSk14Bd0AG2JoP5Zsue1I1DT/VDOFRPAonZg1J7PdlIknsEvDB2srHXV/zCkSr10sBDIOV.PF8f/'
+PROJ_PORT=30000
+PROJ_IP="192.168.64.211"
+FILE_PORT=8000
+CHUNK_SIZE=456976
 
 # kill old process
 line1=$(head -n 1 tmp)
@@ -7,14 +12,19 @@ kill $line1
 kill $line2
 
 # Start hosting the worker script
-python3 -m http.server 8000 -d ../ &
+python3 -m http.server $FILE_PORT -d ../ &
 echo $! > tmp
 
 # Create master worker tasks
-g++ -o master ../src/master.cpp
+g++ -o master.o ../src/master.cpp -pthread -std=c++11
 # Usages: bin port chunk_size hashed_value
-./master 30000 456976 "$6$1UNfGmmL8v6itaPG$j54ho2EN0nnSk14Bd0AG2JoP5Zsue1I1DT/VDOFRPAonZg1J7PdlIknsEvDB2srHXV/zCkSr10sBDIOV.PF8f/" &
+./master.o $PROJ_PORT $CHUNK_SIZE $HASHED &
 echo $! >> tmp
 
 # Deploy a worker script
-sh ../init/deploy-worker.sh
+ansible-playbook -i ../src/ansible/inventory.ini ../src/ansible/deploy-tasks.yml \
+    --extra-vars "proj_ip=$PROJ_IP" \
+    --extra-vars "proj_port=$PROJ_PORT" \
+    --extra-vars "file_port=$FILE_PORT" \
+    --extra-vars "hashed=$HASHED" \
+    --extra-vars "chunk_size=$CHUNK_SIZE"
